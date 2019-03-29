a0c1a1d783bdf7125355653d925f0016ae15b256
chrisjsimpson <chris@karmacomputing.co.uk>
2019-03-29 00:22:50 +0000

    initial setup.py for pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="obp_python",
+    version="0.0.2",
+    author="",
+    author_email="",
+    description="Beta Open Bank Project Python Utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chrisjsimpson/open-bank-project-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['requests'],
+)
