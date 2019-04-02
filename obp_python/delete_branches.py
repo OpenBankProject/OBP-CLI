08918ec4d960bc5c397389268f5111509726c058
chrisjsimpson <chris@karmacomputing.co.uk>
2019-04-02 14:16:58 +0100

    moved delete_branches.py

--- obp_python/delete_branches.py
+++ obp_python/delete_branches.py
@@ -1,12 +1,13 @@
 import requests
 import json
+import os
 
-OBP_ENDPOINT = os.getenv('OBP_ENDPOINT', False)
+OBP_API_HOST = os.getenv('OBP_API_HOST', False)
 OBP_BANK_ID = os.getenv('OBP_BANK_ID', False)
 OBP_AUTH_TOKEN = os.getenv('OBP_AUTH_TOKEN', False)
 
 # Get all branches
-url = "{}/obp/v3.1.0/banks/{}/branches".format(OBP_ENDPOINT, OBP_BANK_ID)
+url = "{}/obp/v3.1.0/banks/{}/branches".format(OBP_API_HOST, OBP_BANK_ID)
 headers = {'Authorization':'DirectLogin token="{}"'.format(OBP_AUTH_TOKEN)}
 response = requests.get(url, headers=headers)
 branches = json.loads(response.text)['branches']
@@ -17,7 +18,7 @@ for branch in branches:
   branch_ids.append(branch['id'])
 
 for branch_id in branch_ids:
-    url = "{}/obp/v3.1.0/banks/{}/branches/{}".format(OBP_ENDPOINT, OBP_BANK_ID, branch_id)
+    url = "{}/obp/v3.1.0/banks/{}/branches/{}".format(OBP_API_HOST, OBP_BANK_ID, branch_id)
     print(url)
     headers = {'Authorization':'DirectLogin token="{}"'.format(OBP_AUTH_TOKEN)}
     response = requests.delete(url, headers=headers)
