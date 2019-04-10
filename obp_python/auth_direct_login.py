bd6c526795a7c2d441bf4c60237b1d7d65ffb368
chrisjsimpson <chris@karmacomputing.co.uk>
2019-04-10 23:00:52 +0100

    #1 store OBP settings in os specific config dir. TODO use these for subsequent requests

--- obp_python/auth_direct_login.py
+++ obp_python/auth_direct_login.py
@@ -1,19 +1,20 @@
 import requests
 import os
 import json
+from .init import get_config
 
-CONSUMER_KEY = os.getenv('CONSUMER_KEY', False)
-OBP_USERNAME = os.getenv('OBP_USERNAME', False)
-PASSWORD = os.getenv('PASSWORD', False)
-OBP_API_HOST = os.getenv('OBP_API_HOST', False)
-def getAuthToken(OBP_USERNAME=OBP_USERNAME, CONSUMER_KEY=CONSUMER_KEY, PASSWORD=PASSWORD, OBP_API_HOST=OBP_API_HOST):
+def getAuthToken(OBP_USERNAME=None, OBP_CONSUMER_KEY=None, OBP_PASSWORD=None, OBP_API_HOST=None):
 
-  if CONSUMER_KEY is False:
-    CONSUMER_KEY = input("CONSUMER_KEY --> ")
-    OBP_USERNAME = input("OBP_USERNAME --> ")
-    PASSWORD = input("PASSWORD --> ")
-    OBP_API_HOST = input("OBP_API_HOST (include http:// or https://) --> ")
-  authorization = 'DirectLogin username="{}", password="{}", consumer_key="{}"'.format(OBP_USERNAME, PASSWORD, CONSUMER_KEY)
+  if OBP_CONSUMER_KEY is None:
+    OBP_CONSUMER_KEY = get_config("OBP_CONSUMER_KEY")
+  if OBP_USERNAME is None:
+    OBP_USERNAME = get_config("OBP_USERNAME")
+  if OBP_PASSWORD is None:
+    OBP_PASSWORD = get_config("OBP_PASSWORD")
+  if OBP_API_HOST is None:
+    OBP_API_HOST = get_config("OBP_API_HOST")
+
+  authorization = 'DirectLogin username="{}", password="{}", consumer_key="{}"'.format(OBP_USERNAME, OBP_PASSWORD, OBP_CONSUMER_KEY)
 
   headers={ 
         'Accept': 'application/json', 
@@ -31,10 +32,10 @@ def getAuthToken(OBP_USERNAME=OBP_USERNAME, CONSUMER_KEY=CONSUMER_KEY, PASSWORD=
 
 
 if __name__ == '__main__':
-  CONSUMER_KEY = input("CONSUMER_KEY --> ")
+  OBP_CONSUMER_KEY = input("OBP_CONSUMER_KEY --> ")
   OBP_USERNAME = input("OBP_USERNAME --> ")
-  PASSWORD = input("PASSWORD --> ")
+  OBP_PASSWORD = input("OBP_PASSWORD --> ")
   OBP_API_HOST = input("OBP_API_HOST --> ")
   print("Your auth token is:")
-  print(getAuthToken(OBP_USERNAME=OBP_USERNAME, CONSUMER_KEY=CONSUMER_KEY, PASSWORD=PASSWORD, OBP_API_HOST=OBP_API_HOST))
+  print(getAuthToken(OBP_USERNAME=OBP_USERNAME, OBP_CONSUMER_KEY=OBP_CONSUMER_KEY, OBP_PASSWORD=OBP_PASSWORD, OBP_API_HOST=OBP_API_HOST))
   
--- obp_python/cli.py
+++ obp_python/cli.py
@@ -1,6 +1,7 @@
 import click
 from .auth_direct_login import getAuthToken
-from .init import init_config_dir, set_obp_api_host, set_obp_username, set_obp_password
+from .init import (init_config_dir, set_obp_api_host, set_obp_username, 
+                  set_obp_password, set_obp_consumer_key, set_obp_auth_token)
 
 @click.group()
 def cli():
@@ -15,10 +16,24 @@ def init():
   set_obp_api_host(OBP_API_HOST)
   OBP_USERNAME = click.prompt("Please enter your username: ")
   set_obp_username(OBP_USERNAME)
-  OBP_PASSWORD = click.prompt("Please enter your password: ", hide_input=True, confirmation_prompt=True)
+  OBP_PASSWORD = click.prompt("Please enter your password: ", hide_input=True,
+                              confirmation_prompt=True)
   set_obp_password(OBP_PASSWORD)
   
   click.echo("... generating direct login token")
+  if click.confirm("Do you have a consumer key?") is False: 
+    click.echo("Consumer key needed to generate a DirectLogin token")
+    if click.confirm("Would you like to generate one?", abort=True):
+      exit("Not implemented. Visit {}, login, click \"Get an api key\" "
+           "and register a consumer. This will give you a consumer key"
+           " which you can use here.".format(OBP_API_HOST))
+  else:
+    OBP_CONSUMER_KEY = click.prompt("Please enter your OBP_CONSUMER_KEY: ")
+    set_obp_consumer_key(OBP_CONSUMER_KEY)
+    
+  authToken = getAuthToken()
+  set_obp_auth_token(authToken)
+
   click.echo("... storing direct login token in user config path")
   click.echo("Init complete")
 
