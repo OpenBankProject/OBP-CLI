136f39456fe40c53d83f2a8e2da371bd9ae8d242
chrisjsimpson <chris@karmacomputing.co.uk>
2019-04-10 13:21:19 +0100

    wip #1  When I type obp init, Then I should see "Please enter your API_HOST". and  When I type in my API_HOST and press the enter key,  Then I should see "Please enter your username" TODO update user story to ask for password

--- obp_python/cli.py
+++ obp_python/cli.py
@@ -5,6 +5,13 @@ from .auth_direct_login import getAuthToken
 def cli():
   pass
 
+
+@cli.command()
+def init():
+  OBP_API_HOST = input("Please enter your API_HOST: ")
+  OBP_USERNAME = input("Please enter your username: ")
+  print("Init complete")
+
 @cli.command()
 def getauth():
   authToken = getAuthToken()
