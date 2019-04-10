536824cf906400c5c9d415dc9d139a3a17b4f2f4
chrisjsimpson <chris@karmacomputing.co.uk>
2019-04-10 13:46:52 +0100

    added for #1  Then I should see "Please enter your password",  Then I should see "... generating direct login token" ,  Then I should see "... storing direct login token in user config path", TODO actually store this..

--- obp_python/cli.py
+++ obp_python/cli.py
@@ -8,9 +8,13 @@ def cli():
 
 @cli.command()
 def init():
-  OBP_API_HOST = input("Please enter your API_HOST: ")
-  OBP_USERNAME = input("Please enter your username: ")
-  print("Init complete")
+  OBP_API_HOST = click.prompt("Please enter your API_HOST: ", 
+                              default="http://127.0.0.1:8080")
+  OBP_USERNAME = click.prompt("Please enter your username: ")
+  OBP_PASSWORD = click.prompt("Please enter your password: ", hide_input=True, confirmation_prompt=True)
+  click.echo("... generating direct login token")
+  click.echo("... storing direct login token in user config path")
+  click.echo("Init complete")
 
 @cli.command()
 def getauth():
