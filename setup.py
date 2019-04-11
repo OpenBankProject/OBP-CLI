7c767082d704933482e716bcc8537a83488bdf87
chrisjsimpson <chris@karmacomputing.co.uk>
2019-04-11 12:39:57 +0100

    version bump 0.12

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="obp_python",
-    version="0.11",
+    version="0.12",
     author="",
     author_email="",
     description="Beta Open Bank Project Python Utilities",
