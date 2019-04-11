0de1be377e13c632eb8128cfff2f5526c1bc4629
chrisjsimpson <chris@karmacomputing.co.uk>
2019-04-11 12:13:55 +0100

    getAuthToken cli client code validates response code success/fail.

--- obp_python/cli.py
+++ obp_python/cli.py
@@ -1,4 +1,5 @@
 import click
+import json
 from .auth_direct_login import getAuthToken
 from .init import (init_config_dir, set_obp_api_host, set_obp_username, 
                   set_obp_password, set_obp_consumer_key, set_obp_auth_token,
@@ -35,12 +36,16 @@ def init():
                                     default=get_config("OBP_CONSUMER_KEY"))
     set_obp_consumer_key(OBP_CONSUMER_KEY)
     
-  authToken = getAuthToken()
-  set_obp_auth_token(authToken)
+  req = getAuthToken()
+  if req.status_code == 201 or req.status_code == 200:
+    authToken = json.loads(req.text)['token']
+    set_obp_auth_token(authToken)
+  else:
+    exit(req.text)
 
   click.echo("Init complete")
 
 @cli.command()
 def getauth():
   authToken = getAuthToken()
-  print(authToken)
+  print(json.loads(authToken.text))
