c992987529e4f7608c53a35f8aa909e7e49334d5
chrisjsimpson <chris@karmacomputing.co.uk>
2019-04-16 22:35:54 +0100

    added command `obp sandboximport --example` which will auto import a minimal dataset using the Sandbox import endpoint

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="obp_python",
-    version="0.14",
+    version="0.15",
     author="",
     author_email="",
     description="Beta Open Bank Project Python Utilities",
