4bee59ce9cdc1547a4ce821a2b4197c504364e7a
chrisjsimpson <chris@karmacomputing.co.uk>
2019-05-15 00:23:52 +0100

    removed unused imports in getbanks

--- obp_python/getAccountsHeld.py
+++ obp_python/getAccountsHeld.py
@@ -1,10 +1,10 @@
 import requests
 from .init import get_config
 
-def getBanks():
+def getAccountsHeld(bank_id):
   
-  url = get_config('OBP_API_HOST') + '/obp/v3.1.0/banks'
-  
+  url = get_config('OBP_API_HOST') + '/obp/v3.1.0/banks/{}/accounts-held'.format(bank_id)
+
   authorization = 'DirectLogin token="{}"'.format(get_config('OBP_AUTH_TOKEN'))
   headers = {'Content-Type': 'application/json',
             'Authorization': authorization}
