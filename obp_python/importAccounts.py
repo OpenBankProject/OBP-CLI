a7667071783a435324c512e57dc9bd6e238c79f5
chrisjsimpson <chris@karmacomputing.co.uk>
2019-06-10 14:52:28 +0100

    wip fetch accounts row by row from sheet, attempt import: TODO get user id based on username in order for apicall to succeed

--- obp_python/importAccounts.py
+++ obp_python/importAccounts.py
@@ -26,13 +26,15 @@ def importAccounts(spreadsheet=None, sheet_name=None):
     import pdb;pdb.set_trace()
     try:
       username = get_value(0, account)
-      label = get_value(1, account)
-      account_type = get_value(1, account)
-      balance_currency = get_value(3, account)
+      bank_id = get_value(1, account)
+      account_id = get_value(2, account)
+      label = get_value(3, account)
+      account_type = get_value(4, account)
+      balance_currency = get_value(5, account)
       balance_amount = 0
-      branch_id = get_value(5, account)
-      account_routing_scheme = get_value(6, account)
-      account_routing_address = get_value(7, account)
+      branch_id = get_value(7, account)
+      account_routing_scheme = get_value(8, account)
+      account_routing_address = get_value(9, account)
       #Build account object
       account = {
         "user_id": "",
@@ -55,19 +57,19 @@ def importAccounts(spreadsheet=None, sheet_name=None):
   #Post accounts to api
   sucessCount = 0
   failCount = 0
-  failedBranches = []
+  failedAccounts = []
   for payload in accounts:
       authorization = 'DirectLogin token="{}"'.format(get_config('OBP_AUTH_TOKEN'))
       headers = {'Content-Type': 'application/json',
                 'Authorization': authorization}
-      url = get_config('OBP_API_HOST') + '/obp/v3.1.0/banks/{}/accounts'.format(bank_id)
+      url = get_config('OBP_API_HOST') + '/obp/v3.1.0/banks/{BANK_ID}/accounts/{ACCOUNT_ID}'.format(BANK_ID=bank_id, ACCOUNT_ID=account_id)
       response = requests.post(url, json=payload, headers=headers)
 
       print(response.text)
       if response.status_code is not 201:
         print(response.text)
         failCount = failCount + 1
-        failedBranches.append(payload)
+        failedAccounts.append(payload)
       elif response.status_code == 201:
         print(response.text)
         sucessCount = sucessCount + 1
@@ -76,7 +78,7 @@ def importAccounts(spreadsheet=None, sheet_name=None):
   print("Success: {}".format(sucessCount))
   print("Failed: {}".format(failCount))
   print("The accounts which failed to import, if any,  were:")
-  for account in failedBranches:
+  for account in failedAccounts:
     print(account)
   print("Success: {}".format(sucessCount))
   print("Failed: {}".format(failCount))
