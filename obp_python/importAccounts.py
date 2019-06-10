d1d2300e103f32baa794f40c3fa46c4b6eaee265
chrisjsimpson <chris@karmacomputing.co.uk>
2019-06-10 16:09:54 +0100

    post all accounts to Open Bank project from sheet and report number of success / failed inserts Fix #6

--- obp_python/importAccounts.py
+++ obp_python/importAccounts.py
@@ -5,6 +5,7 @@ from .init import get_config
 from pyexcel_ods import get_data
 import sys, traceback
 import re
+from .createAccount import createAccount
 
 def importAccounts(spreadsheet=None, sheet_name=None):
   
@@ -21,13 +22,15 @@ def importAccounts(spreadsheet=None, sheet_name=None):
         return obj[index]
       except IndexError:
         return ''
+  sucessCount = 0
+  failCount = 0
+  failedAccounts = []
 
   for index, account in enumerate(sheetdata[1:][0][1:]): #skips sheetname, and header
-    import pdb;pdb.set_trace()
     try:
       username = get_value(0, account)
       bank_id = get_value(1, account)
-      account_id = get_value(2, account)
+      account_id = str(get_value(2, account))
       label = get_value(3, account)
       account_type = get_value(4, account)
       balance_currency = get_value(5, account)
@@ -35,44 +38,42 @@ def importAccounts(spreadsheet=None, sheet_name=None):
       branch_id = get_value(7, account)
       account_routing_scheme = get_value(8, account)
       account_routing_address = get_value(9, account)
-      #Build account object
-      account = {
-        "user_id": "",
-        "label": label,
-        "type": account_type,
-        "balance": {
-          "currency": balance_currency,
-          "amount": balance_amount
-        },
-        "branch_id": branch_id,
-        "account_routing": {
-          "scheme": account_routing_scheme,
-          "address": account_routing_address
-        }
-      }
-      accounts.append(account)
-    except Exception as e:
-      traceback.print_exc(file=sys.stdout)
-
-  #Post accounts to api
-  sucessCount = 0
-  failCount = 0
-  failedAccounts = []
-  for payload in accounts:
+      # Get user id based on usernamse (NOTE: User must exist in order for 
+      # import to succeed). Also, the user making the request must have the 
+      # "CanGetAnyUser" entitlement. If you are a super user, you can use
+      # `obp addrole --role-name CanGetAnyUser` to grant this entitlement to
+      # yourself. 
       authorization = 'DirectLogin token="{}"'.format(get_config('OBP_AUTH_TOKEN'))
       headers = {'Content-Type': 'application/json',
                 'Authorization': authorization}
-      url = get_config('OBP_API_HOST') + '/obp/v3.1.0/banks/{BANK_ID}/accounts/{ACCOUNT_ID}'.format(BANK_ID=bank_id, ACCOUNT_ID=account_id)
-      response = requests.post(url, json=payload, headers=headers)
+      url = get_config('OBP_API_HOST') + '/obp/v3.1.0/users/username/{USERNAME}'.format(USERNAME=username)
+      response = requests.get(url, headers=headers)
+      if response.status_code is 200:
+        user_id = response.json()['user_id']
+      else:
+        user_id = 'user not found'
+
+      #Post accounts to api
+      response = createAccount(bankid=bank_id, userid=user_id, currency=balance_currency, label=label,
+                  type=account_type, balance=balance_amount, branchid=branch_id,
+                  accountid=account_id)
 
       print(response.text)
-      if response.status_code is not 201:
-        print(response.text)
-        failCount = failCount + 1
-        failedAccounts.append(payload)
-      elif response.status_code == 201:
+      if response.status_code is 200:
+        print("WARNING: account aleady exists")
         print(response.text)
         sucessCount = sucessCount + 1
+      elif response.status_code is 201:
+        print(response.text)
+        sucessCount = sucessCount + 1
+      else:
+        print(response.text)
+        failCount = failCount + 1
+        failedAccounts.append(account)
+      
+    except Exception as e:
+      traceback.print_exc(file=sys.stdout)
+
 
 
   print("Success: {}".format(sucessCount))
