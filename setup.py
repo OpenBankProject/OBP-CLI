4e1a288afb098b868bfa4b73d4e6225f1d0bde3e
chrisjsimpson <chris@karmacomputing.co.uk>
2019-06-11 10:41:17 +0100

    version bump to 0.194 (#14)
    * added missing example_import.json to the repo
    
    * versino bump to 0.194

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="obp_python",
-    version="0.193",
+    version="0.194",
     author="",
     author_email="",
     description="Beta Open Bank Project Python Utilities",
