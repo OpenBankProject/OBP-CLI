7be83cc980602074f2c5dab8cf1a21ba804642fb
chrisjsimpson <chris@karmacomputing.co.uk>
2019-06-11 23:44:19 +0100

    wip Issue 15 import transactions from sheet (#17)
    * wip create and import transactions from speadsheet. Currently only creates the transaction request TODO accept the transaction request automatically

---
+++ obp_python/answerTransactionChallenge.py
@@ -0,0 +1,18 @@
+import requests
+import os
+import json
+from .init import get_config
+
+def answerTransactionChallenge(bank_id=None, account_id=None, transation_req_id=None, 
+                challenge_id=None, challenge_type="SANDBOX_TAN"):
+
+    payload = '{"id": "' + challenge_id + '","answer": "123456"}'    #any number works in sandbox mode
+
+    url = get_config('OBP_API_HOST') + '/obp/v3.1.0/banks/{bank_id}/accounts/{account_id}/owner/transaction-request-types/sandbox/transaction-requests/{transation_req_id}/challenge'.format(bank_id=bank_id, account_id=account_id, transation_req_id=transation_req_id, challenge_type=challenge_type)
+    
+    authorization = 'DirectLogin token="{}"'.format(get_config('OBP_AUTH_TOKEN'))
+    headers = {'Content-Type': 'application/json',
+              'Authorization': authorization}
+    req = requests.post(url, headers=headers, json=payload)
+    import pdb;pdb.set_trace()
+    return req
