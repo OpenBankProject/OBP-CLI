844d961c1a5aa299ddd47b912313e47eb5d1d8cb
chrisjsimpson <chris@karmacomputing.co.uk>
2019-06-19 14:07:06 +0100

    wip #43 createCard importCards, and getCustomerIdByCustomerNumber

---
+++ obp_python/importCards.py
@@ -0,0 +1,125 @@
+import requests
+from .init import get_config
+from pyexcel_ods import get_data
+import sys, traceback
+from .createCard import createCard
+from .getCustomerIdByCustomerNumber import getCustomerIdByCustomerNumber
+
+def importCards(spreadsheet=None, sheet_name=None):
+  """
+  Import cards into Open Bank Project
+
+  Required roles: CanCreateCardsForBank
+  e.g. obp addrole --role-name CanCreateCardsForBank --bank-id gh.29.uk.x
+  """
+  
+  OBP_AUTH_TOKEN = get_config('OBP_AUTH_TOKEN')
+  OBP_API_HOST = get_config('OBP_API_HOST')
+
+  sheetdata = get_data(spreadsheet).popitem() #Pops first sheet
+  cards = []
+
+  def get_value(index=None, obj=None):
+      try:
+        return obj[index]
+      except IndexError:
+        return ''
+  sucessCount = 0
+  failCount = 0
+  failedCards = []
+
+  for index, card in enumerate(sheetdata[1:][0][1:]): #skips sheetname, and header
+    try:
+      bank_id = get_value(0, card)
+      card_number = get_value(1, card)
+      card_type = get_value(2, card)
+      name_on_card = get_value(3, card)
+      issue_number = get_value(4, card)
+      serial_number = get_value(5, card)
+      valid_from_date = get_value(6, card)
+      expires_date = get_value(7, card)
+      enabled = get_value(8, card)
+      technology = get_value(9, card)
+      networks = get_value(10, card)
+      allows = get_value(11, card)
+      account_id = get_value(12, card)
+      replacement_requested_date = get_value(13, card)
+      replacement_reason_requested = get_value(14, card)
+      pin_reset_requested_date = get_value(15, card)
+      pin_reset_reason_requested = get_value(16, card)
+      collected = get_value(17, card)
+      posted = get_value(18, card)
+      customer_number = get_value(19, card)
+
+      # Work out customer id
+      req = getCustomerIdByCustomerNumber(bank_id=bank_id,
+                    customer_number=customer_number)
+      if req.status_code == 200 or req.status_code == 201:
+        customer_id = req.json()['customer_id']
+      else:
+        print("ERROR: Could not determin customer_id")
+        print(req)
+        exit(-1)
+
+      #Post card to api
+      response = createCard(bank_id=bank_id, card_number=card_number,
+                  card_type=card_type, name_on_card=name_on_card, issue_number=issue_number,
+                  serial_number=serial_number, valid_from_date=valid_from_date, 
+                  expires_date=expires_date, enabled=enabled, 
+                  technology=technology, networks=networks, allows=allows,
+                  account_id=account_id, 
+                  replacement_requested_date=replacement_requested_date,
+                  replacement_reason_requested=replacement_reason_requested,
+                  pin_reset_requested_date=pin_reset_requested_date,
+                  pin_reset_reason_requested=pin_reset_reason_requested, 
+                  collected=collected, posted=posted,
+                  customer_id=customer_id)
+
+      print(response.text)
+      # Update card number to the number specified in the spreadsheet
+      # We need this because the createCard api call does not store the
+      # card number upon initial creation TODO change this.
+      if response.status_code is 201 or response.status_code is 200:
+        card_id = response.json()['card_id']
+        updateCardNumber(bank_id=bank_id, card_id=card_id, 
+                              card_number=card_number)
+  
+      # Link card to 'username' if username is presnt in sheet
+      if response.status_code is 201 or response.status_code is 200 \
+        and bool(username) is not False:
+        card_id = response.json()['card_id']
+        req = getUserIdByUsername(username=username)
+        if req.status_code is 200:
+          user_id = req.json()['user_id']
+          req = linkUserToCard(bank_id=bank_id, user_id=user_id,
+                                    card_id=card_id)
+          if req.status_code == 400:
+            print(req.text)
+            exit(-1)
+          if req.status_code is not 201:
+            print("WARNING: could not create link between User and Card")
+
+      if response.status_code is 200:
+        print("WARNING: card aleady exists")
+        print(response.text)
+        sucessCount = sucessCount + 1
+      elif response.status_code is 201:
+        print(response.text)
+        sucessCount = sucessCount + 1
+      else:
+        print(response.text)
+        failCount = failCount + 1
+        failedCards.append(card)
+      
+    except Exception as e:
+      traceback.print_exc(file=sys.stdout)
+
+
+
+  print("Success: {}".format(sucessCount))
+  print("Failed: {}".format(failCount))
+  print("The cards which failed to import, if any,  were:")
+  for card in failedCards:
+    print(card)
+  print("Success: {}".format(sucessCount))
+  print("Failed: {}".format(failCount))
