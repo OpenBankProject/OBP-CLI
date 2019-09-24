fcf406968986797396b398c93b1c67b113f1dc5c
tawoe <tobias.woelk@tesobe.com>
2019-09-24 14:38:07 +0200

    added getCustomerKycMedia
    (cherry picked from commit e676ae3ed0b76c3a20e62df912c7704d61d69c6a)

---
+++ obp_python/getCustomerKycMedia.py
@@ -0,0 +1,26 @@
+import requests
+from .init import get_config
+from .hasEntitlements import hasEntitlements
+
+
+def getCustomerKYCMedia(customer_id=None):
+
+    # Validate entitlements
+    requiredEntitlements = ['CanGetKycMedia']
+    fail, msg = hasEntitlements(entitlements_required=requiredEntitlements)
+
+    if fail is True:
+      print(msg)
+      exit(-1)
+    url = get_config('OBP_API_HOST') + '/obp/v4.0.0/customers/{{customer_id}}/kyc_media'.format(bank_id=customer_id)
+
+    authorization = 'DirectLogin token="{}"'.format(get_config('OBP_AUTH_TOKEN'))
+    headers = {'Content-Type': 'application/json',
+              'Authorization': authorization}
+    req = requests.get(url, headers=headers)
+
+    if req.status_code == 403:
+      print(req.text)
+      exit(-1)
+
+    return req
